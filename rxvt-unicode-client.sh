70795195f34667b0ac062ab83df23435acdbba03
Dan Reidy <dubkat@gmail.com>
2015-08-10 08:06:46 -0600

    update rxvt-unicode scripts
    update rxvt-unicode launch scripts to actually work.

--- rxvt-unicode-client.sh
+++ rxvt-unicode-client.sh
@@ -1,16 +1,51 @@
 #!/bin/bash
-
+# automatically use the best rxvt daemon
+# Copyright (C) 2014-2015 Dan Reidy <dubkat@gmail.com>, http://plus.google.com/+DanReidy
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+#################################################################################
 # automatically use the best rxvt client
 # fallback to the basic standalone if available
 
-# urxvtc           urxvtc-256color 
+# urxvtc           urxvtc-256color
 # urxvt            urxvt-256color
 
-RXVTC="/bin/false"
+# the daemon script should be in your path.
+RXVT_DAEMON="rxvt-unicode-daemon.sh"
 
-for x in urxvtc-256color urxvtc urxvt-256color urxvt; 
-do
-    test -x "/usr/bin/${x}" && exec /usr/bin/${x} && exit $?
+function start_server ()
+{
+  test `pgrep urxvtd` || $RXVT_DAEMON
+}
+
+function start_client ()
+{
+  for client in urxvtc-256color urxvtc; do
+    test -x "/usr/bin/${client}" && exec "/usr/bin/${client}" && exit $?
+  done
+}
+
+start_server;
+start_client;
+
+# if we got here, there's something not right.
+for x in urxvt-256color urxvt; do
+  test -x "/usr/bin/${x}" && exec "/usr/bin/${x}" && exit $?
 done
 
-
+err_msg="ERROR: rxvt-unicode-client.sh was unable to launch any terminal. is rxvt-unicode even installed?"
+logger -t rxvt-unicode "$err_msg"
+exit 99
--- rxvt-unicode-daemon.sh
+++ rxvt-unicode-daemon.sh
@@ -1,13 +1,28 @@
  #!/bin/bash
+ # automatically use the best rxvt daemon
+ # Copyright (C) 2014-2015 Dan Reidy <dubkat@gmail.com>, http://plus.google.com/+DanReidy
+ #
+ # This program is free software; you can redistribute it and/or
+ # modify it under the terms of the GNU General Public License
+ # as published by the Free Software Foundation; either version 2
+ # of the License, or (at your option) any later version.
+ #
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ #
+ # You should have received a copy of the GNU General Public License
+ # along with this program; if not, write to the Free Software
+ # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ #
+ #################################################################################
 
-# automatically use the best rxvt daemon
-
-# urxvtd           urxvtd-256color
-
-# no need to get fancy....
-
-killall -u $USER -KILL urxvtd 2>&1 >/dev/null
-killall -u $USER -KILL urxvtd-256color 2>&1 >/dev/null
+# calling this script will always kill any running urxvt daemon before relaunching.
+pid=`pgrep urxvtd`
+if [ $pid ]; then
+  kill -u $USER -KILL $pid >/dev/null 2>&1
+fi
 
 test -x /usr/bin/urxvtd-256color && \
     /usr/bin/urxvtd-256color -f -q -o && \
@@ -18,4 +33,3 @@ test -x /usr/bin/urxvtd && \
     exit $?
 
 exit 1
-
