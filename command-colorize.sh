a879bce9e586e08d8c0d00f73f80c09de19314f4
Dan Reidy <dubkat@zenith.box>
2016-03-07 11:34:20 -0700

    Updated image_rename and command-colorize

---
+++ command-colorize.sh
@@ -0,0 +1,133 @@
+# colorized output of select commands
+# Sparkles & Poniez for all.
+# Dan Reidy <dubkat@gmail.com>
+# https://github.com/dubkat
+
+
+
+if ! hash colout 2>/dev/null; then return; fi
+
+if [ "x${COLORIZE}" != "xyes" ]; then return; fi
+export COLORIZE_VERSION="16.03.1"
+
+# a few things do it for us with no work.
+if hash colordiff 2>/dev/null; then
+	alias diff="`command -v colordiff`"
+fi
+
+if hash colorsvn 2>/dev/null; then
+	alias svn="`command -v colorsvn`"
+fi
+
+
+
+	function df {
+		fsrx='(/[^ ]*)'
+		fs=" (fuseblk|fuse.sshfs|sshfs|hfs|msdos|exfat|autofs|devtmpfs|procfs|devfs|tmpfs|reiserfs\d?|ext[234]|btrfs|xfs|ufs|iso8859) "
+		fsco='Hash'
+		command df $* | colout "$fsrx" white | colout "$fs" Hash Hash | colout ' \b[\d.]*[KMGT0]\b ' cyan rainbow | colout --scale 0,100 '\s*([0-9]{1,3}%)\s*' scale;
+	}
+
+	if hash di 2>/dev/null; then
+		function di {
+			local mnt_opts=
+			if [ x"$1" == x"--mntopts" ]; then
+				mnt_opts="O"
+				shift
+			fi
+			local DI_ARGS="${DI_ARGS}${mnt_opts} $@"
+			command di ${DI_ARGS}  | \
+			     	colout --scale 0,100 \
+        				'^([^ ]+)\s+(/?[^ ]*)\s+([0-9\.]+[GMTK]?)\s+([0-9\.]+[GMTK]?)\s+([0-9\.]+[GMTK]?)\s+(\d+%)\s+([^ ]+)(?:\s+([^ ]+)?)' \
+       	 				Hash,201,yellow,red,green,scale,Hash,white \
+        				Hash,bold,normal,normal,normal,bold,Hash,italic | \
+        			\
+        			colout 'Filesystem.*|Mount|Size|Used|Free|%Used|fs Type|Options.*' \
+        				222 \
+        				reverse
+		}
+	fi
+
+
+	function ps {
+		#command ps $* | colout '(USER|PID|%CPU|%MEM|VSZ|RSS|TT|STAT|STARTED|TIME|COMMAND)' white, underline | colout '^.*$' Random
+		command ps $* | colout '^([ ^]+) .*' Hash Hash
+	}
+
+	function mount {
+		command mount $* | colout '^(\S+) on (\S+) type (\S+) \((.*)\)$' green,cyan,blue,magenta bold
+	}
+
+	#error prone on gentoo
+	#function lsof {
+	#	command lsof $* | colout -a '(COMMAND|PID|DEVICE|USER|FD|TYPE|SIZE/OFF|NODE|NAME)|\b(\d+)\b|(IPv[64])|(TCP)|(UDP)|(\S+):([^ ]+)|(->)|([^ ]+)' \
+	#	white,red,cyan,yellow,blue,cyan,magenta,red,white underline,bold
+	#}
+	function printenv {
+		command printenv $* | sort | colout -a '([^=]*)(=)(.*)' 199,255,214 normal,bold,normal
+	}
+
+	if hash mediainfo 2>/dev/null; then
+		function mediainfo {
+			command mediainfo "$@" | colout  "(.*)\s\s+:(.*)" white,199 bold,bold | colout '^(General|Video|Audio|Menu|Text).*' magenta reverse
+		}
+	fi
+
+	if hash ip 2>/dev/null; then
+		function ip {
+			command ip "$@" | colout '((?:BROAD|MULTI|ANY)CAST)' green normal | colout 'state UNKNOWN' yellow bold | colout 'state UP' green bold | colout '(state) (DOWN)' red,red bold,blink | colout '(link/[^ ]+) ([^ ]+)' cyan,white italic,bold | colout '(brd) ([^ ]+)' white,yellow bold,normal | colout '^(\d+):\s(\S+): .*' white,green | colout '(inet6?) (\S+)' white,green bold,,bold | colout 'scope global' green bold | colout 'scope link' yellow bold | colout 'scope host|NO-CARRIER' red normal | colout 'forever' green normal | colout 'dynamic' magenta normal
+		}
+	fi
+
+	for x in md5{sum,deep,hmac} shasum sha1{sum,deep,hmac} sha224{sum,deep,hmac} sha256{sum,deep,hmac} sha384{sum,deep,hmac} sha512{sum,deep,hmac} whirlpooldeep tigerdeep; do
+		hash_regex='^(?:(.+): (OK)|(.+): (FAILED))'
+		hash_colors="black,green,white,red"
+
+		if hash $x 2>/dev/null; then
+			case $x in
+				md5sum   ) function md5sum 	 { command md5sum $*  	| colout "$hash_regex" "$hash_colors"; } ;;
+				shasum   ) function shasum  	 { command shasum $*  	| colout "$hash_regex" "$hash_colors"; } ;;
+				sha1sum  ) function sha1sum 	 { command sha1sum $* 	| colout "$hash_regex" "$hash_colors"; } ;;
+				sha1hmac ) function sha1hmac 	 { command sha1hmac $* 	| colout "$hash_regex" "$hash_colors"; } ;;
+				sha224sum ) function sha224sum 	 { command sha224sum $* | colout "$hash_regex" "$hash_colors"; } ;;
+				sha256sum ) function sha256sum	 { command sha256sum $*	| colout "$hash_regex" "$hash_colors"; } ;;
+				sha256hmac ) function sha256hmac { command sha256hmac $*| colout "$hash_regex" "$hash_colors"; } ;;
+				sha384sum ) function sha384sum	 { command sha384sum $*	| colout "$hash_regex" "$hash_colors"; } ;;
+				sha384hmac ) function sha384hmac { command sha384hmac $*| colout "$hash_regex" "$hash_colors"; } ;;
+				sha512sum ) function sha512sum	 { command sha512sum $*	| colout "$hash_regex" "$hash_colors"; } ;;
+				sha512hmac ) function sha512hmac { command sha512hmac $*| colout "$hash_regex" "$hash_colors"; } ;;
+			esac
+		fi
+	done
+
+	if hash iptables 2>/dev/null; then
+		function iptables {
+			local ipt_rx_table="^\s*(?:pkts|bytes|target|prot|opt|in|out|source|destination).*\n"; local ipt_co_table="white"
+			local ipt_rx_chain="[Cc]hain ([^ ]+)"; local ipt_co_chain=124
+			local ipt_rx_ref="([0-9]+) references"; local ipt_co_ref=197
+			local ipt_rx_ip="\s([0-9\.:ABCDEFabcdef]+/[0-9]{1,3})\s"; local ipt_co_ip="magenta";
+		        local ipt_rx_bad="(DROP|TARPIT|INVALID)"
+		        local ipt_rx_meh="(REJECT|CHAOS|anywhere|0.0.0.0\/0)"
+       	 		local ipt_rx_ok="(ACCEPT|LOG)"
+			local ipt_rx_return="\s*(RETURN)\s*"; local ipt_co_return="green"
+        		local ipt_rx_port=":(\d+(:?:\d+))"
+        		local ipt_co_bad=197
+        		local ipt_co_meh=203
+        		local ipt_co_ok=118
+        		local ipt_co_port=200
+			command iptables "$@" | colout "${ipt_rx_chain}" "${ipt_co_chain}" bold |
+				colout "${ipt_rx_table}" "${ipt_co_table}" reverse |
+				colout "${ipt_rx_ref}" "${ipt_co_ref}" normal |
+				colout "${ipt_rx_return}" "${ipt_co_return}" normal |
+				colout "${ipt_rx_ip}" "${ipt_co_ip}" bold |
+				colout "${ipt_rx_bad}" "${ipt_co_bad}" bold |
+				colout "${ipt_rx_meh}" "${ipt_co_meh}" bold |
+				colout "${ipt_rx_ok}" "${ipt_co_ok}" |
+				colout "${ipt_rx_port}" "${ipt_co_port}"
+		}
+		function ip6tables {
+			command ip6tables "$@" | colout "$ipt_regex" "$ipt_colors" "$ipt_extras"
+		}
+	fi
+
+
---
+++ image_rename.pl
@@ -0,0 +1,171 @@
+#!/usr/bin/env perl
+
+#^^^ This should point to your system's perl, or which ever
+#    custom perl you may have installed. Usually /usr/bin/perl
+#
+#       AUTHOR: Dan Reidy (dubkat), dubkat@gmail.com
+#     HOMEPAGE: http://google.com/+DanReidy
+#    MORE INFO: https://github.com/dubkat/misc-linux-tools
+#      VERSION: v15.10.07
+#      CREATED: 2015-07-24
+#      LICENSE: GPL-2
+#
+# Perl Critic Level: 3
+#
+# Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS "AS IS'
+# AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES. THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED
+# BY YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR CONTRIBUTOR WILL BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE
+# USE OF THE PACKAGE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# do we have any common sense?
+use if eval { require common::sense } == 1, "common::sense";
+use if eval { require common::sense } == 0, "strict";
+use if eval { require common::sense } == 0, "warnings";
+use Carp;
+use feature qw(say);
+use version; our $VERSION = 'v15.11.18';
+use Image::ExifTool qw(:Public);
+use HTTP::Date;
+use Env qw(HOME);
+
+my $root_path = shift @ARGV;
+my $seq = sprintf("%000d",0);
+my $output_dir = $ENV{'HOME'} ."/Pictures/sorted";
+
+# if we want to make directories for year
+my $group_by_year = 1;
+# auto delete camera generated thumnails.
+my $delete_thumbs = 1;
+
+sub file_processor {
+  my $in_file = shift;
+
+  # if it's a camera thumbnail, handle it as requested.
+  if ($in_file =~ m/\.THM$/x) {
+    if ($delete_thumbs) {
+      unlink $in_file;
+    }
+    return;
+  }
+
+  # skip over any file in the camera that we ourselves dont specifically handle.
+  if ( $in_file !~ m/\.(jpe?g|png|tiff?|webm|webp|mkv|mp4|mov)$/ix ) {
+    return;
+  }
+
+  # whatever's left should be for us.
+
+  say "Processing file: $_";
+  # lets look up it's exif data.
+  my $year;
+  my $output = $output_dir;
+  my $info = ImageInfo($_);
+  my $create = $info->{'CreateDate'};
+  my $type = lc $info->{'FileType'};
+  if ( $type eq "jpeg" ) {
+    $type = "jpg";
+  }
+  my ($date,$time) = split / +/, $create;
+  # lets stop modifying timestamps. most OS's should be able to handle it.
+  #$time =~ s/:/-/gx;
+  $date =~ s/:/-/gx;
+  my $stamp = str2time("$date $time");
+
+
+  my ($sec,$min,$hour,$mday,$mon,$gyear,$wday,$yday,$isdst) = gmtime($stamp);
+  my $media_touch = sprintf "%04d-%02d-%02d %02d:%02d:%02d", $gyear+1900, $mon, $mday, $hour, $min, $sec;
+  #print "DEBUG: stamp($stamp), media_touch($media_touch) \n";
+
+  if ( $group_by_year ) {
+    ($year) = $date =~ (m/(^\d{4})/x);
+    if ( ! -d "$output_dir/$year" ) {
+      qx{ mkdir -p "$output_dir/$year" } or croak "Failed to create directory $output_dir/$year\n";
+    }
+    $output = "$output_dir/$year";
+  }
+
+  $create = sprintf("%s_%s", $date, $time);
+  next if $create eq "_";
+  my $edited = "";
+  my $basename = sprintf("%s/%s", $output, $create);
+
+  if ( $_ =~ m/\-edited\./x ) {
+    $edited = "-edited";
+  }
+
+  if ( $type =~ m/mov|3gp/x ) {
+    $type = 'mp4';
+  }
+
+  my $newfile = sprintf("%s%s.%s", $basename, $edited, $type);
+  while ( -e $newfile ) {
+    my $sequence = sprintf("%04d", $seq);
+    my $new_basename = $basename ."-". $sequence;
+    $newfile = sprintf("%s%s.%s", $new_basename, $edited, $type);
+    $seq++;
+  }
+
+  my @cmd;
+
+  if ( $_ =~ m/\.(jpe?g|png|tiff?|webp)$/ix ) {
+    #printf("source: %s\ndest: %s\n\n", $_, $newfile);
+    qx(mv -v $_ $newfile);
+    #qx(touch -d "$media_touch" $newfile);
+  }
+
+  #rename($_, $newfile);
+  if ($_ =~ /\.(3gp|mov)$/ix ) {
+    my $vinfo = ImageInfo($_);
+    my $vcodec = undef;
+    my $acodec = undef;
+    require Data::Dumper;
+    #print Dumper $info;
+
+    if( $vinfo->{'CompressorID'} eq 'avc1' ) {
+      #video is already mp4 compatable.
+      $vcodec = "copy";
+    } else {
+      $vcodec = "libx264";
+    }
+
+    if ($vinfo->{'AudioFormat'} eq 'aac' ) {
+      $acodec = "-acodec copy";
+    } else {
+      $acodec = "-strict -2";
+    }
+    qx(ffmpeg -i $_ -map 0 -metadata creation_time='$media_touch' -c:v $vcodec $acodec $newfile);
+  }
+  #system(@cmd);
+  $seq = 1;
+}
+
+
+sub directory_processor {
+  my $path = shift;
+  $path =~ s/\/$//;
+
+  opendir (DIR, $path) or die "Unable to open $path: $!\n";
+  my @files = grep { !/^\./ } readdir (DIR);
+  closedir(DIR);
+  # prepend the pathname to files.
+  @files = map { $path . '/' . $_ } @files;
+
+  for (@files) {
+    if ( -d $_ ) {
+      directory_processor($_);
+      next;
+    }
+    file_processor($_);
+  }
+}
+
+
+if ( ! -d $root_path ) { croak "First arguement must be a directory."; }
+if ( ! -d $output_dir ) {
+  qx{mkdir -p $output_dir} || croak "Failed to create output directory.";
+}
+
+print "Attempting to open $root_path\n";
+directory_processor($root_path);
