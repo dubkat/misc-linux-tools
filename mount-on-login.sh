723379d8bde6b6322edb4836962bdb5a185ba762
Dan Reidy <dubkat+github@gmail.com>
2016-05-11 01:00:42 -0600

    mount-on-login

--- mount-on-login.sh
+++ mount-on-login.sh
@@ -5,7 +5,7 @@
 ## zenith.box: /home/dubkat/Zenith fuse.sshfs  noauto,users,user=1005,follow_symlinks,transform_symlinks,rw,fsname=fuse 0  0
 ## dubkat      /home/dubkat/.cache   tmpfs       noauto,nofail,user  0 0
 
-for point in $(grep -E "$USER|user=$UID" /etc/fstab | awk '{ print $2 }'); do
+for point in $(grep -v '^#' /etc/fstab | grep -E "$USER|user=$UID"  | awk '{ print $2 }'); do
         if ! mountpoint -q "${point}"; then
                 mount "${point}" ||:
         fi
