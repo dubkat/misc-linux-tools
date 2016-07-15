f5fd491777e0db830290784bf5f266ac73265512
Dan Reidy <dubkat+github@gmail.com>
2016-07-15 16:34:56 -0600

    variable changes

--- usr:local/etc/bashrc.d/20-setup.sh
+++ usr:local/etc/bashrc.d/20-setup.sh
@@ -12,18 +12,14 @@ if [ ! -z "$TERM" ]; then
 fi
 
 
-if [ $UID -gt 0 ]; then
-        eval `make_user_tmpdir`
-        #eval `gcc-flags.sh`
-fi
-
 eval `generate_path`
+eval `make_user_tmpdir`
+#eval `gcc-flags.sh`
 
-
-if [ -n $DONT_TOUCH_MY_PROMPT ]; then
+if [ -z "$ULE_DONT_TOUCH_MY_PROMPT" ]; then
   export PS1="\[\033[$(unique_user_color)m\]\u\[\033[00;38;5;155;01m\]@\[\033[${MACHINE_COLOR}m\]\h\[\033[00;38;5;195m\] \w\n\[\033[00;38;5;155;01m\]$\[\033[00m\] "
 fi
 
-if [ -n $SILENT_WELCOME ]; then
+if [ -z "$ULE_SILENT_WELCOME" ]; then
   test -x ${ULE_SETTING['BIN_DIR']}/system-welcome.sh && ${ULE_SETTING['BIN_DIR']}/system-welcome.sh
 fi
