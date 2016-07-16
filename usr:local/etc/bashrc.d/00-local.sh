140bbe388fc1232e72fdceebdf77316fd21d4c8d
Dan Reidy <dubkat+github@gmail.com>
2016-07-15 19:16:04 -0600

    renamed some core files

---
+++ usr:local/etc/bashrc.d/00-local.sh
@@ -0,0 +1,16 @@
+# set your global config options here.
+# you can optionally set/override them
+# in your own ~/.bashrc
+# here are some example's you may find important.
+
+# silence the welcome by setting this to 1
+# ULE_SILENT_WELCOME=0
+
+# dont touch my prompt, if this messes with your own custom prompt, set it.
+# ULE_DONT_TOUCH_MY_PROMPT=0
+
+# Machine Color: this can also be set in /etc/machine-info
+# see the output from "unique_host_color".
+# MACHINE_COLOR=
+
+
