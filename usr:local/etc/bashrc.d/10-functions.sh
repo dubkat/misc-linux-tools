da060e620fea0b57b4eaa998ccfc3c3f5ba2c498
Dan Reidy <dubkat@gmail.com>
2016-07-20 07:27:54 +0000

    add commandline shortcut to the Xepher X display.

--- usr:local/etc/bashrc.d/10-functions.sh
+++ usr:local/etc/bashrc.d/10-functions.sh
@@ -1,49 +1,50 @@
 # 99-functions.sh
 # Copyright (C) 2015-2016 Dan Reidy <dubkat@gmail.com>
-ULE_VERSION['functions']=16.07.15
+ULE_VERSION['functions']=16.07.20
 export ULE_RUNTIME=4
 
 function _ls ()
 {
-    local IFS=' ';
-    command ls $LS_OPTIONS ${1+"$@"}
+  local IFS=' ';
+  command ls $LS_OPTIONS ${1+"$@"}
 }
 
 ule_version() {
-cat<<EOF
-Copyright 2015-$(date +%Y) Dan Reidy <dubkat@gmail.com>
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  cat<<EOF
+  Copyright 2015-$(date +%Y) Dan Reidy <dubkat@gmail.com>
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-usr:local module versions:
+  usr:local module versions:
 EOF
-        for x in $ULE_MODULES; do
-                printf "%12s: %s\n" $x ${ULE_VERSION[$x]}
-        done
+
+  for x in $ULE_MODULES; do
+    printf "%12s: %s\n" $x ${ULE_VERSION[$x]}
+  done
 
 }
 
 ule_uname() {
-        if [ ! -z "$@" ]; then
-                command uname $@
-                return $?
-        fi
-        local opts="nodename machine processor hardware-platform operating-system kernel-name kernel-release kernel-version";
-        for x in $opts; do
-                printf "* %20s: %s\n" "${x}" "$(uname --${x})";
-        done
+  if [ ! -z "$@" ]; then
+    command uname $@
+    return $?
+  fi
+  local opts="nodename machine processor hardware-platform operating-system kernel-name kernel-release kernel-version";
+  for x in $opts; do
+    printf "* %20s: %s\n" "${x}" "$(uname --${x})";
+  done
 }
 
 # generate a random whole number between 0 and X (255 by default)
 rand() {
-	local ceil=${1:-255}
-	case $ceil in
-		*[0-9]* )
-			perl -le '{ print int(rand(shift)); }' $ceil;
-			;;
-	esac
+  local ceil=${1:-255}
+  case $ceil in
+    *[0-9]* )
+    perl -le '{ print int(rand(shift)); }' $ceil;
+    ;;
+  esac
 }
 
 
@@ -71,42 +72,42 @@ generate_path() {
 # your mac address, or hostname. Thus each machine you have
 # should have very unique colors.
 unique_host_color() {
-	if ! hash colout 2>/dev/null; then
-		echo -n "1;38;5;$(rand)"
-	fi
-	local string;
-	local device;
-	local escape;
-	# based on mac address
-	# we look for the direct path ourselves as /sbin is likely not in
-	# our users path.
-	if [ -x "/sbin/ip" ]; then
-		device=$(command /sbin/ip route show 0.0.0.0/0 | awk '{ print $5 }');
-		string=$(command /sbin/ip addr show $device | grep 'link/ether' | awk '{ print $2 }');
-	elif [ -x "/sbin/ifconfig" -a -x "/sbin/route" ]; then
-		device="$(command /sbin/route|grep default|awk '{ print $NF }')"
-		string="$(command /sbin/ifconfig $device|grep HWaddr|awk '{ print $NF }'|tr '[A-Z]' '[a-z]' )"
-	elif hash hostnamectl 2>/dev/null; then
-		string="$(command hostnamectl --static)"
-	else
-		string="$(command hostname -f)"
-	fi
-	if [ -z "$string" ]; then
-		escape="1;38;5;$(rand)"
-	else
-		escape="$(echo $string | colout '^.*$' Hash | cat -v | grep -Po '[01];38;5;\d+')"
-	fi
-	echo -n "$escape"
+  if ! hash colout 2>/dev/null; then
+    echo -n "1;38;5;$(rand)"
+  fi
+  local string;
+  local device;
+  local escape;
+  # based on mac address
+  # we look for the direct path ourselves as /sbin is likely not in
+  # our users path.
+  if [ -x "/sbin/ip" ]; then
+    device=$(command /sbin/ip route show 0.0.0.0/0 | awk '{ print $5 }');
+    string=$(command /sbin/ip addr show $device | grep 'link/ether' | awk '{ print $2 }');
+  elif [ -x "/sbin/ifconfig" -a -x "/sbin/route" ]; then
+    device="$(command /sbin/route|grep default|awk '{ print $NF }')"
+    string="$(command /sbin/ifconfig $device|grep HWaddr|awk '{ print $NF }'|tr '[A-Z]' '[a-z]' )"
+  elif hash hostnamectl 2>/dev/null; then
+    string="$(command hostnamectl --static)"
+  else
+    string="$(command hostname -f)"
+  fi
+  if [ -z "$string" ]; then
+    escape="1;38;5;$(rand)"
+  else
+    escape="$(echo $string | colout '^.*$' Hash | cat -v | grep -Po '[01];38;5;\d+')"
+  fi
+  echo -n "$escape"
 }
 
 unique_user_color() {
   local escape;
-	if ! hash colout 2>/dev/null; then
-		escape="1;38;5;`rand`";
-	else
-	   escape="$(echo $USER | colout '^.*$' Hash | cat -v | grep -Po '[01];38;5;\d+')"
+  if ! hash colout 2>/dev/null; then
+    escape="1;38;5;`rand`";
+  else
+    escape="$(echo $USER | colout '^.*$' Hash | cat -v | grep -Po '[01];38;5;\d+')"
   fi
-	echo -n "$escape"
+  echo -n "$escape"
 }
 
 genpasswd() {
@@ -162,7 +163,7 @@ session_id() {
   if hash uuid 2>/dev/null; then
     uuid=$(uuid -v1);
   elif hash uuidgen 2>/dev/null; then
-	   uuid=$(uuidgen -t);
+    uuid=$(uuidgen -t);
   else
     uuid="$(random.sh 192 2>/dev/null | tr '/' '+')"
   fi
@@ -172,17 +173,17 @@ session_id() {
 
 # create user designated tmpdir location, if it doesn't exist.
 function make_user_tmpdir() {
-  
+
 
   if [ -n "$TMPDIR" ]; then
-  	if [ "$TMPDIR" = "/tmp" ]; then
-  		unset TMPDIR
-  		make_user_tmpdir
-  	fi
+    if [ "$TMPDIR" = "/tmp" ]; then
+      unset TMPDIR
+      make_user_tmpdir
+    fi
     return
   fi
 
-   if [ -d "/tmp" -a -x "/tmp" ]; then
+  if [ -d "/tmp" -a -x "/tmp" ]; then
     TMPDIR="/tmp/${USER}"
   elif [ -d "/var/tmp" -a -x "/var/tmp" ]; then
     TMPDIR="/var/tmp/${USER}"
@@ -193,8 +194,8 @@ function make_user_tmpdir() {
   fi
 
   if [ ! -d "${TMPDIR}" ]; then
-  	mkdir -p "${TMPDIR}"
-  	chmod 700 "${TMPDIR}"
+    mkdir -p "${TMPDIR}"
+    chmod 700 "${TMPDIR}"
   fi
   export TMPDIR=${TMPDIR}
   echo export TMPDIR=${TMPDIR}
@@ -204,24 +205,41 @@ function vman {
   /usr/bin/man $* | col -bp | iconv -c | view -c 'set ft=man nomod nolist' -
 }
 
-#random_mac() {
-#  echo "Switching off radio for mac address change."
-#  ifconfig wlp2s0 down
-#  macchanger -A wlp2s0
-#  macchanger -e wlp2s0
-#  ifconfig wlp2s0 up
-#  echo "macchange complete"
-#}
+function xephyr {
+  local winmode=win
+  local host=broadcast
+  if ! hash Xephyr 2>/dev/null; then
+    echo "The program Xephyr is not installed. You can probably find it among " >&2
+    echo "your Xorg packages." >&2
+    return 1
+  fi
+  if [ ${#@} -gt 2 ]; then
+    echo "usage: xephyr <window|fullscreen> [host]" >&2
+    exit 1
+  fi
+  for arg in $@; do
+    case $arg in
+      win|window) winmode=win; ;;
+      fs|fullscreen|full) winmode=fs; ;;
+      broadcast|broad|bc|255.255.255.255) host=broadcast; ;;
+      * ) host="$arg"; ;;
+    esac
+  done
 
-#default_mac() {
-#  local default=00:00:d8:00:00:01
-#  ifconfig wlp2s0 down
-#  if [ ${#default} ]; then
-#    macchanger -mac="$default" wlp2s0
-#  else
-#    macchanger --permanent wlp2s0
-#  fi
-#  ifconfig wlp2s0 up
-#}
+  xephyr="Xephyr -br -ac -noreset -fp tcp/localhost:7100 +iglx "
+  if [ "$winmode" = "win" ]; then
+    xephyr+="-screen 1280x720 "
+  elif [ "$windmode" = "fs" ]; then
+    xephyr+="-fullscreen "
+  fi
+  if [ "$host" = "broadcast" ]; then
+    xephyr+="-broadcast "
+  else
+    xephyr+="-query $host "
+  fi
+  xephyr+=":1 "
+  $xephyr >/dev/null 2>&1 &
+  return $?
+}
 
 unset ULE_RUNTIME
