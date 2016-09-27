75bd2185ec41b0539d33cc3102b479a097ee650c
Dan Reidy <dubkat@gmail.com>
2016-09-27 07:02:41 -0600

    move gcc-symlinks to its rightful home: crap.

--- crap/gcc-symlinks.sh
+++ crap/gcc-symlinks.sh
@@ -3,18 +3,19 @@
 # create symlinks to gcc/binutils based on local chost (eg. x86_64-suse-linux-gnu-gcc)
 # requires: RPM
 
-cmd="ln"
+cmd="echo ln "
 if [ $UID -ne 0 ]; then
         echo "You must be $(getent -i passwd 0 | awk -F: '{ print $1 }') to run this script."
         cmd="echo ln"
 fi
 
-target=$(rpm -E %{_target_platform}%{?_gnu});
+#target=$(rpm -E %{_target_platform}%{?_gnu});
+target=$(rpm -E %{_target_platform});
 bindir=$(rpm -E %_bindir);
-linkdir="/usr/local/libexec"
+linkdir="/usr/bin"
 
 package_list=
-for package in gcc gcc-c++ gcc-fortran gcc5 gcc5-c++ gcc5-fortran gcc6 gcc6-c++ gcc6-fortran cpp binutils binutils-gold make; do 
+for package in gcc gcc-c++ gcc-fortran gcc5 gcc5-c++ gcc5-fortran gcc6 gcc6-c++ gcc6-fortran cpp; do
         package_list+="$(rpm -ql $package 2>/dev/null | grep ${bindir} | xargs) "
 done
 
@@ -30,3 +31,5 @@ for package in $package_list; do
 done
 
 echo "export PATH=${linkdir}:$PATH"
+
+# update-alternatives --install 
