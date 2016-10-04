fa13e57eb06861350fe264ec3e2cc71fe926a3de
Dan Reidy <dubkat@gmail.com>
2016-10-04 16:41:53 -0600

    darwinify

--- nano/install.sh
+++ nano/install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+owner=root
+group=root
+if [ "`uname -o`" = "Darwin" ]; then
+	group=wheel
+fi
+
+
 if test $UID -gt 0; then
     echo "This script will install the accompaning .nanorc files into the system folder." >&2
     echo "This process requires you to be root. Try running this as: sudo $0" >&2
@@ -8,7 +15,7 @@ fi
 
 if [ ! -r /etc/nanorc ]; then
     echo "Installing default nanorc to /etc/nanorc"
-    install -o root -g root -m 644 nanorc /etc/nanorc
+    install -o $owner -g $group -m 644 nanorc /etc/nanorc
 fi
 
 LIST="`command dir $(dirname $0)/*.nanorc`"
@@ -28,7 +35,7 @@ fi
 
 for config in $LIST; do
     echo "Installing ${config##*/} to $dest";
-    install -o root -g root -m 644 $config $dest ||:
+    install -o $owner -g $group -m 644 $config $dest ||:
 done
 
 echo "Installation complete."
